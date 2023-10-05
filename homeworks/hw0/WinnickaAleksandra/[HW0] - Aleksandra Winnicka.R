60c51afaae21bb1e7ee09c00fb0e920cdb3d514d
winnicka <147074438+winnicka@users.noreply.github.com>
2023-10-05 17:51:54 +0200

    {HW0] - Aleksandra Winnicka

---
+++ homeworks/hw0/WinnickaAleksandra/[HW0] - Aleksandra Winnicka.R
@@ -0,0 +1,28 @@
+
+
+install.packages("proton")
+library(proton)
+proton()
+
+#Etap 1
+colnames(employees)
+employees{employees$surname=="insecure",}
+proton(action="login", login="johnins")
+
+
+#Etap2
+proton(action)="login", login="johnins",
+      password="ABC")
+top1000passwords
+
+for (pass in top1000passwords) {
+  response <- proton(action="login", login="johnins",
+                     password=pass)
+  if (response=="Success! User is logges in!"){
+  }
+}
+
+
+#Etap3
+
+employees[employees$surname=="Pietraszko"]
