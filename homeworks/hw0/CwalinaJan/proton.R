516273196563b0f2e5e24528a59ac3f4dc81c9d6
Janusz99bis <147074104+Janusz99bis@users.noreply.github.com>
2023-10-07 14:27:58 +0200

    proton.R

--- homeworks/hw0/CwalinaJan/proton.R
+++ homeworks/hw0/CwalinaJan/proton.R
@@ -1,5 +1,6 @@
 #install.packages("proton")
-#library(proton)
+library(proton)
+library(stringi)
 #proton()
 
 #1
@@ -11,7 +12,12 @@ for (i in top1000passwords) {
   proton(action = "login", login="johnins", password=i)
 }
 
-#
+#3
 employees[employees$name=="Pietraszko" | employees$surname=="Pietraszko",]
 head(sort(table(logs[logs$login=="slap",c(2)]),decreasing = TRUE))
 proton(action = "server", host="194.29.178.16")
+
+#4
+for (i in unique(stri_extract(bash_history,regex = "\\w+"))) {
+  proton(action = "login", login="slap", password=i)
+}
