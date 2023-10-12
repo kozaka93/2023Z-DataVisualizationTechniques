e10aa86714df9658743b034cba34f032351b635c
woznicak <kaswoz4@gmail.com>
2023-10-12 12:50:19 +0200

    hw1 - tasks

---
+++ homeworks/hw1/HW1_tasks.R
@@ -0,0 +1,30 @@
+library(tidyr)
+library(dplyr)
+
+df <- read.csv('~/Downloads/spotify/spotify-2023.csv')
+
+
+#### 1.Jakie jest średnia liczba odtworzeń piosenek wypuszczonych w roku 2023 w pierwszym kwartale?
+
+
+#### 2. Czy piosenki stworzone przez więcej niż 2 artystów są zawarte na większej liczbie playlist spotify 
+#### niż piosenki stworzone przez 1 lub 2 artystów?
+
+
+#### 3. Jaki jest najpopularniejszy dzień tygodnia w wypuszczaniu piosenek?
+
+#### 4. Który artysta zanotował największy procentowy wzrost liczby wypuszczonych piosenek w 2022 względem roku 2021? (ogranicz się tylko do utworów w których był jeden wykonawca)
+
+
+#### 5. Spośród piosenek znajdujących się w 10% najbardziej tanecznych, piosenka którego artysty  ma średnio najwięcej odtworzeń na rok? Załóżmy, że piosenki wypuszczone w 2023 były dostępne przez cały jedne rok 
+
+#### 6. Jakim średnim tempem i najczęściej występującą tonacją ('mode') charakteryzują się piosenki, 
+#### które są w 8 decylu najczęściej odtwarzanych piosenek w przeliczeniu na liczbę playlist spotify?
+
+#### 7. Jakie charakterystyki taneczności, energetyki itd. mają piosenki publikowane w poszczególnych porach roku?
+
+#### 8.Dla 10 najpopularniejszych par key-mode w roku 2022 wybierze tę którą najcześcej tworzą artyści solowi.
+
+#### 9. Który artysta (osobno) najwięcej odtworzeń wszystkich jego piosenek w sumie?
+
+#### 10. Który artysta średnio generuje więcej odtworzeń na piosenkę gdy tworzy solo niż gdy tworzy z innymi artystami?
